9e650f1ba7937baa7b0615ef5dbf0ad17f3c0130
Abe White <abewhite@mac.com>
2024-01-30 15:17:04 -0600

    Add ComposeView usage and other minor revisions

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         .library(name: "HelloSkipApp", type: .dynamic, targets: ["HelloSkip"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.7.25"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "0.0.0")
+        .package(url: "https://source.skip.tools/skip.git", from: "0.7.48"),
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "0.4.5")
     ],
     targets: [
         .target(name: "HelloSkip", dependencies: (zero ? [] : [.product(name: "SkipUI", package: "skip-ui")]), resources: [.process("Resources")], plugins: skipstone),
--- Sources/HelloSkip/ContentView.swift
+++ Sources/HelloSkip/ContentView.swift
@@ -11,7 +11,7 @@ public struct ContentView: View {
             VStack {
                 Text("Welcome Skipper!")
                 Image(systemName: "heart.fill")
-                    .foregroundColor(.red)
+                    .foregroundStyle(.red)
             }
             .font(.largeTitle)
             .tabItem { Label("Welcome", systemImage: "heart.fill") }
@@ -31,10 +31,19 @@ public struct ContentView: View {
             }
             .tabItem { Label("Home", systemImage: "house.fill") }
 
-            Form {
-                Text("Settings")
-                    .font(.largeTitle)
-                Toggle("Option", isOn: $setting)
+            NavigationStack {
+                Form {
+                    Toggle("Option", isOn: $setting)
+                    #if SKIP
+                    ComposeView { ctx in
+                        androidx.compose.material3.Text("Powered by Jetpack Compose", modifier: ctx.modifier, color: Color.gray.colorImpl())
+                    }
+                    #else
+                    Text("Powered by SwiftUI")
+                        .foregroundStyle(.gray)
+                    #endif
+                }
+                .navigationTitle("Settings")
             }
             .tabItem { Label("Settings", systemImage: "gearshape.fill") }
         }
