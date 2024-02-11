88f5fbda55d7201a615af90705d1529dc8c542c8
marcprux <mwp1@cornell.edu>
2024-02-11 14:46:54 -0500

    Update project template

--- scripts/diff_template.sh
+++ scripts/diff_template.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
-DIR=${1:-"skipapp-hello"}
-AGAINST=${2:-"skipapp-hello"}
+#!/bin/sh -e
+PACKAGE=${1:-"skipapp-hello"}
+AGAINST=${2:-"."}
 
-skip init --no-build --appid=skip.hello.App --version 1.0.0 ${DIR} HelloSkip
+TMPDIR=`mktemp -d`
+cd ${TMPDIR}
+skip init --no-build --appid=skip.hello.App --version 1.0.0 ${PACKAGE} HelloSkip
+cd -
 
 # make sure that the repo exactly matches the template,
 # allowing for changes in some files, as well as
 # dependency versions
-diff --exclude=README.md --exclude=.build --exclude=.git --exclude=scripts --exclude=.github --exclude=.gitignore --exclude=Package.resolved --exclude=Skip.env --exclude=project.xcworkspace --exclude=proguard-rules.pro -I 'url: "https://source.skip.tools/' -r ${DIR} ${AGAINST}
+diff --exclude=README.md --exclude=.build --exclude=.git --exclude=scripts --exclude=.github --exclude=.DS_Store --exclude=.gitignore --exclude=Package.resolved --exclude=Skip.env --exclude=project.xcworkspace --exclude=proguard-rules.pro -I 'url: "https://source.skip.tools/' -r ${TMPDIR}/${PACKAGE} ${AGAINST}
 
