e424251a3d2e09922edf0a74b74a931c253c2d74
Marc Prud'hommeaux <marc@skip.tools>
2025-04-10 09:25:33 -0400

    Update whitespace for diff validation

--- scripts/diff_template.sh
+++ scripts/diff_template.sh
@@ -5,10 +5,11 @@ AGAINST=${2:-"."}
 TMPDIR=`mktemp -d`
 cd ${TMPDIR}
 ${SKIPCMD:-skip} init --no-build --zero --appid=skip.hello.App --version 1.0.0 ${PACKAGE} HelloSkip
+
 cd -
 
 # make sure that the repo exactly matches the template,
 # allowing for changes in some files, as well as
 # dependency versions
-diff --ignore-all-space --exclude=README.md --exclude=.build --exclude=.git --exclude=scripts --exclude=.github --exclude=.DS_Store --exclude=.gitignore --exclude='*.png' --exclude=Package.resolved --exclude=Skip.env --exclude=CHANGELOG.md --exclude=project.xcworkspace -I 'url: "https://source.skip.tools/' -r ${TMPDIR}/${PACKAGE} ${AGAINST}
+diff --ignore-space-change --exclude=README.md --exclude=.build --exclude=.git --exclude=scripts --exclude=.github --exclude=.DS_Store --exclude=.gitignore --exclude='*.png' --exclude=Package.resolved --exclude=Skip.env --exclude=CHANGELOG.md --exclude=project.xcworkspace -I 'url: "https://source.skip.tools/' -r ${TMPDIR}/${PACKAGE} ${AGAINST}
 
