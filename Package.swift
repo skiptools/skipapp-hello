8956ceaf87844d45cbe8c9796afd48af0e353d1e
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-01-08 17:28:55 -0500

    Update project template and dependencies

--- Package.swift
+++ Package.swift
@@ -1,10 +1,6 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // This is a Skip (https://skip.tools) package.
 import PackageDescription
-
-// Set SKIP_ZERO=1 to build without Skip libraries
-let zero = Context.environment["SKIP_ZERO"] != nil
-let skipstone = !zero ? [Target.PluginUsage.plugin(name: "skipstone", package: "skip")] : []
 
 let package = Package(
     name: "skipapp-hello",
@@ -15,13 +11,48 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "1.6.35"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.44.5")
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "HelloSkip", dependencies: (zero ? [] : [
+        .target(name: "HelloSkip", dependencies: [
             .product(name: "SkipUI", package: "skip-ui")
-        ]), resources: [.process("Resources")], plugins: skipstone),
+        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "HelloSkipTests", dependencies: [
-            "HelloSkip"] + (zero ? [] : [.product(name: "SkipTest", package: "skip")]), resources: [.process("Resources")], plugins: skipstone),
+            "HelloSkip",
+            .product(name: "SkipTest", package: "skip")
+        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
+
+// Setting the SKIP_ZERO=1 environment will strip out the Skip plugin and all Skip dependencies
+if Context.environment["SKIP_ZERO"] ?? "0" != "0" {
+    package.targets.forEach { target in
+        // remove the Skip plugin
+        target.plugins?.removeAll(where: {
+            if case .plugin(let name, _) = $0 {
+                return name == "skipstone"
+            } else {
+                return false
+            }
+        })
+
+        // remove the Skip target dependencies
+        target.dependencies.removeAll(where: { dependency in
+            if case .productItem(_, let package, _, _) = dependency {
+                return package == "skip" || package?.hasPrefix("skip-") == true
+            } else {
+                return false
+            }
+        })
+    }
+
+    // remove the Skip package dependencies
+    package.dependencies.removeAll(where: { dependency in
+        if case .sourceControl(_, let url, _) = dependency.kind {
+            return url.hasPrefix("https://source.skip.tools/")
+        } else {
+            return false
+        }
+    })
+}
+
